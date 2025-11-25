394b807d6e6d57be10cd1d74533f809d8fd627e9
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-11-24 17:12:05 -0800

    optional local storage

--- game-files/saves/scores/save_system.gd
+++ game-files/saves/scores/save_system.gd
@@ -12,45 +12,47 @@ var high_scores_dict : Dictionary[String, int] = {}
 # Names of the top scorers, sorted by score.
 var sorted_names : Array[String] = []
 
+var use_local_storage : bool = false
 func _ready():
-	LocalStorage.set_item("TEST", "JACKIE")
-	print(LocalStorage.get_item("TEST"))
-	build_scores()
+	if use_local_storage == true:
+		LocalStorage.set_item("TEST", "JACKIE")
+		print(LocalStorage.get_item("TEST"))
+		build_scores()
 
 
-# Counts entries, and loads variables.
-func initialize_list():
-	var json_scores = JSON.new()
-	json_scores.parse(LocalStorage.get_item("scores"))
-	var score_data = json_scores.data
-	# Add each name and score to a dictionary.
-	for entry in score_data:
-		var score_name = str(entry)
-		var score_value = int(score_data[score_name])
-		high_scores_dict[score_name] = score_value
-		num_entries += 1
-	
-	# Build array of scorers sorted by score level.
-	var blah = JSON.new()
-	blah.parse(LocalStorage.get_item("sorted"))
-	sorted_names = blah.data
-	for key in high_scores_dict.keys():
-		if sorted_names.size() == 0:
-			sorted_names.append(key)
-		else:
-			var size_before : int = sorted_names.size()
-			for i in range(sorted_names.size()):
-				var comparison_entry = sorted_names[i]
-				if high_scores_dict[comparison_entry] <= high_scores_dict[key]:
-					sorted_names.insert(i,key)
-					break
-			if size_before == sorted_names.size():
-				if sorted_names.size() < ENTRY_LIMIT:
-					# List is less than ENTRY_LIMIT, just append to the end.
-					sorted_names.append(key)
-				else:
-					# Couldn't add, not enough space.
-					pass
+## Counts entries, and loads variables.
+#func initialize_list():
+	#var json_scores = JSON.new()
+	#json_scores.parse(LocalStorage.get_item("scores"))
+	#var score_data = json_scores.data
+	## Add each name and score to a dictionary.
+	#for entry in score_data:
+		#var score_name = str(entry)
+		#var score_value = int(score_data[score_name])
+		#high_scores_dict[score_name] = score_value
+		#num_entries += 1
+	#
+	## Build array of scorers sorted by score level.
+	#var blah = JSON.new()
+	#blah.parse(LocalStorage.get_item("sorted"))
+	#sorted_names = blah.data
+	#for key in high_scores_dict.keys():
+		#if sorted_names.size() == 0:
+			#sorted_names.append(key)
+		#else:
+			#var size_before : int = sorted_names.size()
+			#for i in range(sorted_names.size()):
+				#var comparison_entry = sorted_names[i]
+				#if high_scores_dict[comparison_entry] <= high_scores_dict[key]:
+					#sorted_names.insert(i,key)
+					#break
+			#if size_before == sorted_names.size():
+				#if sorted_names.size() < ENTRY_LIMIT:
+					## List is less than ENTRY_LIMIT, just append to the end.
+					#sorted_names.append(key)
+				#else:
+					## Couldn't add, not enough space.
+					#pass
 # A version of the initialize_list() function that uses built in functions in its algorithm.
 func build_scores():
 	# Parse current saved scores
