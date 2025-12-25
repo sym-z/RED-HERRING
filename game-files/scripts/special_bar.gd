a4a5be60964b530f62263c9897b691f43338d754
Jack Sims <123803257+sym-z@users.noreply.github.com>
2025-12-25 00:02:15 -0700

    better special effect

--- game-files/scripts/special_bar.gd
+++ game-files/scripts/special_bar.gd
@@ -1,20 +1,43 @@
 extends TextureProgressBar
 @export var player :Node2D
 var rotation_speed : float = 2.
+var using_special : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player.connect("combo_updated", update_bar)
 	player.connect("special_ready", update_bar)
-	player.connect("use_special", update_bar)
+	#player.connect("use_special", update_bar)
+	player.connect("use_special", special_effect)
 	pass # Replace with function body.
 
-
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("SPECIAL") and player.consecutive_hits >= player.minimum_special_charge:
+		using_special = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	rotation += rotation_speed * delta * player.consecutive_hits
+	#print(using_special)
 	pass
 	
 func update_bar():
 	#print(player.consecutive_hits/player.minimum_special_charge)
-	value = min(100.,(float(player.consecutive_hits)/float(player.minimum_special_charge)) * 100.)
+	print("using special = ", using_special)
+	if using_special == false:
+		print("RESETTING")
+		value = min(100.,(float(player.consecutive_hits)/float(player.minimum_special_charge)) * 100.)
+	else:
+		print("blocked reset")
+	pass
+
+func special_effect():
+	var tween : Tween = create_tween()
+	tween.bind_node(self)
+	tween.tween_property(self,"scale",scale*5, 1.0)
+	tween.tween_callback(reset_special_effect)
+	tween.play()
+	pass
+func reset_special_effect():
+	scale = Vector2(1.0,1.0)
+	using_special = false
+	update_bar()
 	pass
